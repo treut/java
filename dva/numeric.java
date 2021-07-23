a762afb33131528ebf56e6fca9f3e58e276bd25d
treut <d@treut.ru>
2021-07-24 00:29:31 +0300

    from notebook

---
+++ dva/numeric.java
@@ -0,0 +1,15 @@
+import static java.lang.System.*;
+
+public class numeric{
+	public static void main(String args[]){
+		int testa=199;
+		String dimax="Dimax";
+		out.println("My var: "+testa+"*2="+(testa*2));
+		//testa*=2;
+		//System.out.println("New var: "+testa);
+		out.println("My name is "+dimax);
+		if(dimax=="Dimax"){
+
+		}
+	}
+}
---
+++ forer/forer.java
@@ -0,0 +1,11 @@
+public class forer{
+	public static void main(String args[]){
+		long b=999000000000L;
+		double dou=3.54*222.84475;
+		for(int a=0; a<20; a++){
+			System.out.print("\n"+a);
+		}
+		System.out.println("\n"+b);
+		System.out.println("DOU: "+Math.tan(2));
+	}
+}
