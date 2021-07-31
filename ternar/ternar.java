9295245ee03dfedb077932fc9cb8e1451aefd795
treut <d@treut.ru>
2021-07-31 16:47:22 +0300

    add example ternar operator in JAVA

---
+++ ternar/ternar.java
@@ -0,0 +1,17 @@
+class ternar{
+	public static void main(String args[]){
+		int strin=0;
+		System.out.println((strin!=0 ? "aaa " : "bbb "));
+
+		switch(strin){
+			case 1:{
+				System.out.println("GOOD");
+			}
+			break;
+			case 0:{
+				System.out.println("BAAD");
+			}
+			break;
+		}
+	}
+}
