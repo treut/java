bf6db80cd05772a7c4627b6b96296cf499f9d9e0
treut <d@treut.ru>
2021-08-02 09:02:11 +0300

    java-class construntor with this

---
+++ tconstr/tconstr.java
@@ -0,0 +1,21 @@
+class box{
+	int a;
+	int b;
+
+	box(int a, int b){
+		this.a=a;
+		this.b=b;
+	}
+
+	int result(){
+		return a*b;
+	}
+}
+
+class tconstr{
+	public static void main(String args[]){
+		box vbox=new box(7, 7);
+
+		System.out.println("result: "+vbox.result());
+	}
+}
