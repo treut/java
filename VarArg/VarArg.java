9da209f0de2623d130440b186643a356eeed5ae0
treut <d@treut.ru>
2021-08-04 23:32:43 +0300

    change VarArg.java -> add variants vararg

--- VarArg/VarArg.java
+++ VarArg/VarArg.java
@@ -1,8 +1,12 @@
 //varargs
+//04 aug 23-00 changed (add 2, 3 variant)
+//+OverloadVarArg=overload
 
 class VarArg{
 
-	static void set(int v[]){
+	//static void set(int v[]){ //VARIANT 1
+	//static void set(int ... v){ //VARIANT 2
+	static void set(int a, int ... v){ //VARIANT 3
 		System.out.println("count el: "+v.length);
 		System.out.print("res: ");
 		for(int x:v){
@@ -11,18 +15,25 @@ class VarArg{
 		System.out.println();
 	}
 
-		static void anytext(String aaa){
-			System.out.println(aaa);
-		}
 	public static void main(String args[]){
 
+		//FOR VARIANT 1 OR 2
 		int n1[]={111};
-		int n2[]={1,2,3};
+		int n2[]={1, 2, 3};
 		int n3[]={};
 
+
+		//FOR VARIANT 1 OR 2
+		/*
 		set(n1);
-		anytext("alabalabala");
-		//set(n2);
-		//set(n3);
+		set(n2);
+		set(n3);
+		*/
+
+
+		//FOR VARIANT 2 OR 3
+		set(222);
+		set(6, 7, 8);
+		set(888);
 	}
 }
