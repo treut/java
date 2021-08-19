a2f672f77903f004fb2b0ef07ef9d95f4a785b42
treut <d@treut.ru>
2021-08-19 22:04:41 +0300

    mod getString

--- General/JavaSql.java
+++ General/JavaSql.java
@@ -13,11 +13,11 @@ public class JavaSql{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection conn=DriverManager.getConnection(url, user, pass);
 			Statement state=conn.createStatement();
-			ResultSet res=state.executeQuery("SELECT*FROM cards LIMIT 3");
+			ResultSet res=state.executeQuery("SELECT*FROM cards LIMIT 10");
 			
 			while(res.next()){
 				//System.out.println(res.getString(3));
-				System.out.println("Famila: "+res.getString("person")+"\nLogana: "+res.getString("trperson")+"\nCardas: "+res.getString("card")+"\n");
+				System.out.println("Famila: "+res.getString("person")+"\nLogana: "+res.getString("trperson")+"\nCardas: "+res.getString("card")+"\n-------------------------");
 			}
 
 			res.close();
