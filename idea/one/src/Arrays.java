58be4fb73158a05fc54f38d4f63f7e098e0976da
treut <d@treut.ru>
2021-11-01 02:14:44 +0300

    add idea java projects

---
+++ idea/one/src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args){
+//        int[] numbs={16, 44, 55, 65, 45, 34, 65, 76};
+        int[] numbs=new int[2];
+        numbs[0]=33;
+        numbs[1]=44;
+        
+        for(int num:numbs){
+            System.out.println(num);
+        }
+    }
+}
