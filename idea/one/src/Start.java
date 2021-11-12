79b956a73bd11a9b56d924e68acbb76e8cafee7c
treut <d@treut.ru>
2021-11-13 01:27:18 +0300

    add Enum

--- idea/one/src/Start.java
+++ idea/one/src/Start.java
@@ -1,5 +1,22 @@
 public class Start {
     public static void main(String[] args){
-        System.out.println("SSSSSSSS");
+
+//        Enama enama = Enama.ONE;
+//        switch(enama){
+//            case ONE:
+//                System.out.println("this one");
+//                break;
+//            case TWO:
+//                System.out.println("this two");
+//                break;
+//            case THREE:
+//                System.out.println("this three");
+//                break;
+//        }
+
+        Enama dono = Enama.TWO;
+//        System.out.println(dono.getContent());
+        System.out.println(dono);
+        System.out.println(Enama.valueOf("ONE"));
     }
 }
