d61786976aa23f5cc7896897ce4e212207011def
treut <d@treut.ru>
2021-11-22 21:15:41 +0300

    setters

---
+++ idea/one/src/Setters.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Setters {
+    public static void main(String[] args){
+        Set<String> hs = new HashSet<>();
+        Set<String> lhs = new LinkedHashSet<>();
+        Set<String> ts = new TreeSet<>();
+
+        hs.add("ton");
+        hs.add("don");
+        hs.add("gon");
+        hs.add("bon");
+//        hs.add("gon");
+
+        for(String name : hs){
+            System.out.println("Naaame: "+name);
+        }
+
+        System.out.println(hs.contains("don"));
+    }
+}
