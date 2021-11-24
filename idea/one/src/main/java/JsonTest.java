d5b0c135474c6fd80563468dd1cf0bd4a2e1b93a
treut <d@treut.ru>
2021-11-25 00:47:10 +0300

    json read

--- idea/one/src/main/java/JsonTest.java
+++ idea/one/src/main/java/JsonTest.java
@@ -1,7 +1,9 @@
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
@@ -15,23 +17,28 @@ public class JsonTest {
     public static void main(String[] args){
 
 
+        List<String> c = new ArrayList<>();
         JSONArray ja = new JSONArray();
-        ja.put(new JSONObject().put("phone", "УК ЖК Волга Лайф\nтел: +7-920-685-20-50\ne-mail: uk-volga-life@yandex.ru\n\nПн, Вт, Ср, Пт - 8:00-10:30, 15:30-17:00\nЧт - 16:30-20:00\nСб, Вс - выходной"));
-        ja.put(new JSONObject().put("phone", "Аварийно-диспетчерская служба (круглосуточно)\nтел: +7-4822-75-21-56"));
-        ja.put(new JSONObject().put("phone", "Городская клиническая больница №7 (регистратура)\nтел: +7-4822-555-209 , +7-4822-555-055"));
-        ja.put(new JSONObject().put("phone", "Служба безопасности ЖК\nтел: +7-920-697-12-63"));
-        ja.put(new JSONObject().put("phone", "Обслуживание лифтов Ваш Лифт (круглосуточно)\nтел: +7-4822-77-30-35"));
-        ja.put(new JSONObject().put("phone", "Участковый ЖК: Ильин Владислав Сергеевич\nтел: +7-999-160-08-78"));
-        ja.put(new JSONObject().put("phone", "Инженер по эксплуатации: Тимофеев Михаил Александрович\nтел: +7-930-160-12-55"));
-        ja.put(new JSONObject().put("phone", "Бухгалтер ЖК Ирина Викторовна\nтел: +7-930-159-55-83"));
-        ja.put(new JSONObject().put("phone", "Специалист паспортного стола: Соловьева Светлана Николаевна\nтел: +7-900-010-56-99\ne-mail: 89000105699@mail.ru\n\n(прием граждан: Среда 15:30-17:00)"));
+
+        c.add("УК ЖК Волга Лайф\nтел: +7-920-685-20-50\ne-mail: uk-volga-life@yandex.ru\n\nПн, Вт, Ср, Пт - 8:00-10:30, 15:30-17:00\nЧт - 16:30-20:00\nСб, Вс - выходной");
+        c.add("Аварийно-диспетчерская служба (круглосуточно)\nтел: +7-4822-75-21-56");
+        c.add("Городская клиническая больница №7 (регистратура)\nтел: +7-4822-555-209 , +7-4822-555-055");
+        c.add("Служба безопасности ЖК\nтел: +7-920-697-12-63");
+        c.add("Обслуживание лифтов Ваш Лифт (круглосуточно)\nтел: +7-4822-77-30-35");
+        c.add("Участковый ЖК: Ильин Владислав Сергеевич\nтел: +7-999-160-08-78");
+        c.add("Инженер по эксплуатации: Тимофеев Михаил Александрович\nтел: +7-930-160-12-55");
+        c.add("Бухгалтер ЖК Ирина Викторовна\nтел: +7-930-159-55-83");
+        c.add("Специалист паспортного стола: Соловьева Светлана Николаевна\nтел: +7-900-010-56-99\ne-mail: 89000105699@mail.ru\n\n(прием граждан: Среда 15:30-17:00)");
+        for(String cs : c){
+            ja.put(new JSONObject().put("phone", cs));
+        }
 
 
 //        System.out.println(ja.getJSONObject(2).getString("phone"));
 
 
-//        System.out.println();
-//        JSONObject jjo = new JSONObject();
+//        System.out.println(ja);
+
         for(Object jj : ja){
             JSONObject jjo = (JSONObject) jj;
 //            System.out.println(jjo.getString("phone"));
@@ -51,17 +58,10 @@ public class JsonTest {
 //        var jss = new JSONArray(arr);
 //        System.out.println(jss.getJSONArray(1));
 
-        try{
-            InputStream fromurl = new URL("https://tver-life.ru/api/v2/getContacts.php?v=1.1.4").openStream();
-            System.out.println(fromurl.toString());
-        }
-        catch(Exception e){}
+//        InputStream fromurl;
+//        int read;
 
-
-
-
-//        System.out.println(resu.get(2));
-
+    System.out.println(new ReadURL("https://tver-life.ru/api/v2/getContacts.php?v=1.1.4"));
 
     }
 }
---
+++ idea/one/src/main/java/ReadURL.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class ReadURL{
+    String url;
+    public ReadURL(String url){
+        this.url=url;
+    }
+    @Override
+    public String toString(){
+        try(BufferedReader buff = new BufferedReader(new InputStreamReader(new URL(url).openStream()))){
+            String read;
+            StringBuilder result = new StringBuilder();
+            while((read = buff.readLine()) != null){
+                result.append(read);
+            }
+            return result.toString();
+        }
+        catch(Exception e){
+            return "error";
+        }
+    }
+}
